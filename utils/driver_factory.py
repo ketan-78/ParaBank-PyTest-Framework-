ab1a286b3d4a595169aa7a943907f7d72db23998
Ketan Chauhan <166690988+ketan-78@users.noreply.github.com>
2026-06-16 16:04:49 +0530

    Update driver_factory.py

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -16,6 +16,9 @@ def get_driver():
     if browser == "chrome":
         options = webdriver.ChromeOptions()
         options.add_argument("--start-maximized")
+        options.add_argument("--headless=new")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=options)
 
     elif browser == "firefox":
